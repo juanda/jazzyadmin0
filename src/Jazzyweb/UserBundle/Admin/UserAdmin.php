53834845b7c9fd5bd6e6d86f4941cf715c2aeb91
Juan David Rodriguez <juandalibaba@gmail.com>
2013-11-05 18:52:45 +0100

    Cambio la clase que define la administración del usuario para que el campo password sea obligatorio. Para ello defino mi propio UserAdmin y lo incorporo al container mediante el servicio fwuser.admin.user. Entonces cambio en la configuración de sonata (config.yml, seccion sonata_admin ...) el servicio sonata.user.admin.user por el recien creado fwuser.admin.user.
    Observación: La clase que implementa el servicio fwuser.admin.user es
    en  realidad una extensión de Sonata\UserBundle\Admin\Model\UserAdmin con
    la función configureFormFields() cambiada para que el campo password sea
    obligatorio. Esto es un ejemplo de como modificar y reusar código sin tocar
    para nada el código original

---
+++ src/Jazzyweb/UserBundle/Admin/UserAdmin.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: juanda
+ * Date: 5/11/13
+ * Time: 18:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Jazzyweb\UserBundle\Admin;
+
+use Sonata\AdminBundle\Form\FormMapper;
+
+use Sonata\UserBundle\Admin\Model\UserAdmin as BaseUserAdmin;
+
+class UserAdmin extends BaseUserAdmin {
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('General')
+            ->add('username')
+            ->add('email')
+            ->add('plainPassword', 'text', array('required' => true))
+            ->end()
+            ->with('Groups')
+            ->add('groups', 'sonata_type_model', array('required' => false, 'expanded' => true, 'multiple' => true))
+            ->end()
+            ->with('Profile')
+            ->add('dateOfBirth', 'birthday', array('required' => false))
+            ->add('firstname', null, array('required' => false))
+            ->add('lastname', null, array('required' => false))
+            ->add('website', 'url', array('required' => false))
+            ->add('biography', 'text', array('required' => false))
+            ->add('gender', 'sonata_user_gender', array(
+                'required' => true,
+                'translation_domain' => $this->getTranslationDomain()
+            ))
+            ->add('locale', 'locale', array('required' => false))
+            ->add('timezone', 'timezone', array('required' => false))
+            ->add('phone', null, array('required' => false))
+            ->end()
+            ->with('Social')
+            ->add('facebookUid', null, array('required' => false))
+            ->add('facebookName', null, array('required' => false))
+            ->add('twitterUid', null, array('required' => false))
+            ->add('twitterName', null, array('required' => false))
+            ->add('gplusUid', null, array('required' => false))
+            ->add('gplusName', null, array('required' => false))
+            ->end()
+        ;
+
+        if ($this->getSubject() && !$this->getSubject()->hasRole('ROLE_SUPER_ADMIN')) {
+            $formMapper
+                ->with('Management')
+                ->add('realRoles', 'sonata_security_roles', array(
+                    'expanded' => true,
+                    'multiple' => true,
+                    'required' => false
+                ))
+                ->add('locked', null, array('required' => false))
+                ->add('expired', null, array('required' => false))
+                ->add('enabled', null, array('required' => false))
+                ->add('credentialsExpired', null, array('required' => false))
+                ->end()
+            ;
+        }
+
+        $formMapper
+            ->with('Security')
+            ->add('token', null, array('required' => false))
+            ->add('twoStepVerificationCode', null, array('required' => false))
+            ->end()
+        ;
+    }
+
+}
